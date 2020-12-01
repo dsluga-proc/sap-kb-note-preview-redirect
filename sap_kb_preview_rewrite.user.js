78620a668a7ca175c0a435d933e0631e03d036f7
dsluga-proc <54098351+dsluga-proc@users.noreply.github.com>
2020-12-01 15:55:18 +0100

    Initial upload

---
+++ sap_kb_preview_rewrite.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name        Redirect SAP Note Preview
+// @namespace   https://github.com/dsluga-proc/sap-kb-note-preview-redirect
+// @downloadURL https://github.com/dsluga-proc/sap-kb-note-preview-redirect/raw/master/sap_kb_preview_rewrite.user.js
+// @description Redirect SAP Knowledgebase note preview to actual note page
+// @version     0.1
+// @include		https://apps.support.sap.com/sap/support/knowledge/*
+// ==/UserScript==
+
+const currUrl = window.location.href;
+const regex = /[^/]{0,11}$/g;
+const noteNr = currUrl.match(regex)[0];
+
+window.location.replace("https://launchpad.support.sap.com/#/notes/" + noteNr);
