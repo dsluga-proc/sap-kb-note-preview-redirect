6d736d59a6584254935391b7a6066f41e08f93b9
dsluga-proc <54098351+dsluga-proc@users.noreply.github.com>
2023-01-03 12:05:20 +0100

    Update sap_kb_preview_rewrite.user.js

--- sap_kb_preview_rewrite.user.js
+++ sap_kb_preview_rewrite.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/dsluga-proc/sap-kb-note-preview-redirect
 // @downloadURL https://github.com/dsluga-proc/sap-kb-note-preview-redirect/raw/main/sap_kb_preview_rewrite.user.js
 // @description Redirect SAP Knowledgebase note preview to actual note page
-// @version     0.1
+// @version     0.2
 // @match		http*://apps.support.sap.com/sap/support/knowledge/*
 // @match		http*://userapps.support.sap.com/sap/support/knowledge/*
 // @match		http*://service.sap.com/sap/support/notes/*
