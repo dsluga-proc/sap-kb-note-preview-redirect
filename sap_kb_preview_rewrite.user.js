aebcc72d3c5c54924f8d80dba7e9986621a72171
dsluga-proc <54098351+dsluga-proc@users.noreply.github.com>
2023-01-03 12:05:06 +0100

    added old service.sap.com links, updated to use match instead of include

--- sap_kb_preview_rewrite.user.js
+++ sap_kb_preview_rewrite.user.js
@@ -4,7 +4,9 @@
 // @downloadURL https://github.com/dsluga-proc/sap-kb-note-preview-redirect/raw/main/sap_kb_preview_rewrite.user.js
 // @description Redirect SAP Knowledgebase note preview to actual note page
 // @version     0.1
-// @include		https://apps.support.sap.com/sap/support/knowledge/*
+// @match		http*://apps.support.sap.com/sap/support/knowledge/*
+// @match		http*://userapps.support.sap.com/sap/support/knowledge/*
+// @match		http*://service.sap.com/sap/support/notes/*
 // ==/UserScript==
 
 const currUrl = window.location.href;
